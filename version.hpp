01c684cda1a0c30322f8803d35ec7de786a086a5
flakeforever <flakeforeverdev@gmail.com>
2023-07-08 13:51:07 +0800

    update to 1.1.2

--- version.hpp
+++ version.hpp
@@ -14,7 +14,7 @@
 #pragma once
 
 #define PROJECT_NAME "dns-gateway"
-#define PROJECT_VER  "1.0.1"
+#define PROJECT_VER  "1.1.2"
 #define PROJECT_VER_MAJOR "1"
-#define PROJECT_VER_MINOR "0"
-#define PTOJECT_VER_PATCH "1"
+#define PROJECT_VER_MINOR "1"
+#define PTOJECT_VER_PATCH "2"
